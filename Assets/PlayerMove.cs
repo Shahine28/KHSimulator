b1cf2481f3a10e1325deb86106659e880c5ef0cc
Shahine <shahine.benthanane@edu.devinci.fr>
2023-11-10 19:01:55 +0100

    Début du dev de mouvement

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using static UnityEngine.InputSystem.InputSettings;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -22,17 +23,43 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] UnityEvent _onEvent;
     [SerializeField] UnityEvent _onEventPost;
 
+    [SerializeField] private Animator _animator;
+
     public Vector2 JoystickDirection { get; private set; }
     Coroutine MovementRoutine { get; set; }
 
+    [SerializeField] private Rigidbody _rb;
+
     private void Start()
     {
-        
+        _move.action.started += StartMove;
+        _move.action.performed += UpdateMove;
+        _move.action.canceled += StopMove;
+
     }
+
+    private void StartMove(InputAction.CallbackContext context)
+    {
+        OnStartMove?.Invoke();
+        _animator.SetBool("Walking", true);
+    }
+
+    private void UpdateMove(InputAction.CallbackContext context)
+    {
+        _rb.velocity = new Vector3(_move.action.ReadValue<Vector2>().x * _speed, _rb.velocity.y, _move.action.ReadValue<Vector2>().y * _speed);
+    }
+
+    private void StopMove(InputAction.CallbackContext context)
+    {
+        _animator.SetBool("Walking", false);
+    }
+
 
     private void OnDestroy()
     {
-        
+        _move.action.started -= StartMove;
+        _move.action.performed -= UpdateMove;
+        _move.action.canceled -= StopMove;
     }
 
 }
